0ab42246fc15b5a4f0cb82f823c7063624b84e02
zahramansoor <zahramansoor@github.com>
2020-05-28 16:17:12 -0400

    rat brain mods

--- run_parameter_sweep.py
+++ run_parameter_sweep.py
@@ -76,65 +76,64 @@ def sweep_parameters_cluster(jobid, rBP_size_r, fEMP_hmax_r, fEMP_size_r, fEMP_t
     pth = out0+"/parametersweep_rBP_size{}_fEMP_hmax{}_fEMP_size{}_fEMP_threshold{}_fIP_method{}_fIP_size{}_dCSP_threshold{}.tif".format(rBP_size,
         fEMP_hmax, fEMP_size, fEMP_threshold, fIP_method, fIP_size, dCSP_threshold)
 
-    if not os.path.exists(pth):
-        try:
-            #set params for sweep
-            kwargs["removeBackgroundParameter_size"] = (rBP_size,rBP_size) #Remove the background with morphological opening (optimised for spherical objects), e.g. (7,7)
-            kwargs["findExtendedMaximaParameter_hmax"] = fEMP_hmax # (float or None)     h parameter (for instance 20) for the initial h-Max transform, if None, do not perform a h-max transform
-            kwargs["findExtendedMaximaParameter_size"] = fEMP_size # size in pixels (x,y) for the structure element of the morphological opening
-            kwargs["findExtendedMaximaParameter_threshold"] = fEMP_threshold # (float or None)     include only maxima larger than a threshold, if None keep all local maxima
-            kwargs["findIntensityParameter_method"] =  fIP_method # (str, func, None)   method to use to determine intensity (e.g. "Max" or "Mean") if None take intensities at the given pixels
-            kwargs["findIntensityParameter_size"] = (fIP_size,fIP_size,fIP_size) # (tuple)             size of the search box on which to perform the *method*
-            kwargs["detectCellShapeParameter_threshold"] = dCSP_threshold # (float or None)      threshold to determine mask. Pixels below this are background if None no mask is generated
+    try:
+        #set params for sweep
+        kwargs["removeBackgroundParameter_size"] = (rBP_size,rBP_size) #Remove the background with morphological opening (optimised for spherical objects), e.g. (7,7)
+        kwargs["findExtendedMaximaParameter_hmax"] = fEMP_hmax # (float or None)     h parameter (for instance 20) for the initial h-Max transform, if None, do not perform a h-max transform
+        kwargs["findExtendedMaximaParameter_size"] = fEMP_size # size in pixels (x,y) for the structure element of the morphological opening
+        kwargs["findExtendedMaximaParameter_threshold"] = fEMP_threshold # (float or None)     include only maxima larger than a threshold, if None keep all local maxima
+        kwargs["findIntensityParameter_method"] =  fIP_method # (str, func, None)   method to use to determine intensity (e.g. "Max" or "Mean") if None take intensities at the given pixels
+        kwargs["findIntensityParameter_size"] = (fIP_size,fIP_size,fIP_size) # (tuple)             size of the search box on which to perform the *method*
+        kwargs["detectCellShapeParameter_threshold"] = dCSP_threshold # (float or None)      threshold to determine mask. Pixels below this are background if None no mask is generated
 
-            #tmp
-            nkwargs = load_kwargs(kwargs["outputdirectory"])
-            kwargs["outputdirectory"] = out0
-            nkwargs.update(kwargs)
-            pckloc=out0+"/param_dict.p"; pckfl=open(pckloc, "wb"); pickle.dump(nkwargs, pckfl); pckfl.close()
+        #tmp
+        nkwargs = load_kwargs(kwargs["outputdirectory"])
+        kwargs["outputdirectory"] = out0
+        nkwargs.update(kwargs)
+        pckloc=out0+"/param_dict.p"; pckfl=open(pckloc, "wb"); pickle.dump(nkwargs, pckfl); pckfl.close()
 
-            #run cell detection
-            sys.stdout.write("\n\n\n           *****Iteration {} of {}*****\n\n\n".format(jobid, tick))
-            sys.stdout.write("    Iteration parameters: {}     {}     {}     {}     {}     {}     {}".format(kwargs["removeBackgroundParameter_size"], kwargs["findExtendedMaximaParameter_hmax"], kwargs["findExtendedMaximaParameter_size"], kwargs["findExtendedMaximaParameter_threshold"],         kwargs["findIntensityParameter_method"],         kwargs["findIntensityParameter_size"],        kwargs["detectCellShapeParameter_threshold"]))
-            celldetection_operations(optimization_chunk, testing=True, **kwargs)
+        #run cell detection
+        sys.stdout.write("\n\n\n           *****Iteration {} of {}*****\n\n\n".format(jobid, tick))
+        sys.stdout.write("    Iteration parameters: {}     {}     {}     {}     {}     {}     {}".format(kwargs["removeBackgroundParameter_size"], kwargs["findExtendedMaximaParameter_hmax"], kwargs["findExtendedMaximaParameter_size"], kwargs["findExtendedMaximaParameter_threshold"],         kwargs["findIntensityParameter_method"],         kwargs["findIntensityParameter_size"],        kwargs["detectCellShapeParameter_threshold"]))
+        celldetection_operations(optimization_chunk, testing=True, **kwargs)
 
-            #list, load, and maxip
-            raw = [xx for xx in listdirfull(out0+"/optimization/raw") if "~" not in xx and ".db" not in xx]; raw.sort();
-            raw_im = np.squeeze(tifffile.imread(raw))
-            raw_mx = np.max(raw_im, axis = 0)
-            bkg = [xx for xx in listdirfull(out0+"/optimization/background") if "~" not in xx and "Thumbs.db" not in xx]; bkg.sort()
-            bkg_im = tifffile.imread(bkg)
-            bkg_mx = np.max(bkg_im, axis = 0)
-            cell = [xx for xx in listdirfull(out0+"/optimization/cell") if "~" not in xx and ".db" not in xx]; cell.sort()
-            cell_im = tifffile.imread(cell)
-            cell_mx = np.max(cell_im, axis = 0)
+        #list, load, and maxip
+        raw = [xx for xx in listdirfull(out0+"/optimization/raw") if "~" not in xx and ".db" not in xx]; raw.sort();
+        raw_im = np.squeeze(tifffile.imread(raw))
+        raw_mx = np.max(raw_im, axis = 0)
+        bkg = [xx for xx in listdirfull(out0+"/optimization/background") if "~" not in xx and "Thumbs.db" not in xx]; bkg.sort()
+        bkg_im = tifffile.imread(bkg)
+        bkg_mx = np.max(bkg_im, axis = 0)
+        cell = [xx for xx in listdirfull(out0+"/optimization/cell") if "~" not in xx and ".db" not in xx]; cell.sort()
+        cell_im = tifffile.imread(cell)
+        cell_mx = np.max(cell_im, axis = 0)
 
-            #optional rescale:
-            if rescale:
-                raw_mx = rescale_intensity(raw_mx, in_range=str(raw_mx.dtype),
-                    out_range=rescale).astype(rescale)
-                bkg_mx = rescale_intensity(bkg_mx, in_range=str(bkg_mx.dtype),
-                    out_range=rescale).astype(rescale)
-                cell_mx = rescale_intensity(cell_mx, in_range=str(cell_mx.dtype),
-                    out_range=rescale).astype(rescale)
+        #optional rescale:
+        if rescale:
+            raw_mx = rescale_intensity(raw_mx, in_range=str(raw_mx.dtype),
+                out_range=rescale).astype(rescale)
+            bkg_mx = rescale_intensity(bkg_mx, in_range=str(bkg_mx.dtype),
+                out_range=rescale).astype(rescale)
+            cell_mx = rescale_intensity(cell_mx, in_range=str(cell_mx.dtype),
+                out_range=rescale).astype(rescale)
 
-            #concatenate and save out:
-            bigim = np.concatenate((raw_mx, bkg_mx, cell_mx), axis=1)
-            del bkg, bkg_im, bkg_mx, cell, cell_im,cell_mx
-            if cleanup: removedir(out0)
-            if not cleanup: tifffile.imsave(pth, bigim, compress=1)
+        #concatenate and save out:
+        bigim = np.concatenate((raw_mx, bkg_mx, cell_mx), axis=1)
+        del bkg, bkg_im, bkg_mx, cell, cell_im,cell_mx
+        if cleanup: removedir(out0)
+        if not cleanup: tifffile.imsave(pth, bigim, compress=1)
 
-            #save in main
-            npth = out+"/jobid_{}_rBPSize{}_fEMPHmax{}_fEMPSize{}_fEMPThreshold{}_fIPMethod{}_fIPSize{}_dCSPThreshold{}.tif".format(str(jobid).zfill(4), rBP_size, fEMP_hmax, fEMP_size, fEMP_threshold, fIP_method, fIP_size, dCSP_threshold)
-            tifffile.imsave(npth, bigim.astype("uint16"), compress = 1)
+        #save in main
+        npth = out+"/jobid_{}_rBPSize{}_fEMPHmax{}_fEMPSize{}_fEMPThreshold{}_fIPMethod{}_fIPSize{}_dCSPThreshold{}.tif".format(str(jobid).zfill(4), rBP_size, fEMP_hmax, fEMP_size, fEMP_threshold, fIP_method, fIP_size, dCSP_threshold)
+        tifffile.imsave(npth, bigim.astype("uint16"), compress = 1)
 
-        except Exception as e:
-            print("Error on: {}\n\nerror={}".format(pth,e))
-            im = np.zeros((10,10,10))
-            tifffile.imsave(pth, im, compress = 1)
-            with open(os.path.join(out, "errored_files.txt"), "a") as fl:
-                fl.write("\n\n{}\n{}\n".format(pth, kwargs))
-                fl.close
+    except Exception as e:
+        print("Error on: {}\n\nerror={}".format(pth,e))
+        im = np.zeros((10,10,10))
+        tifffile.imsave(pth, im, compress = 1)
+        with open(os.path.join(out, "errored_files.txt"), "a") as fl:
+            fl.write("\n\n{}\n{}\n".format(pth, kwargs))
+            fl.close
 
     return
 #%%
